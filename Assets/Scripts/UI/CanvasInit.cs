d490bc3b2cb788e301a6e91d8cd842bfa7b44e24
Emily <93992216+em-i-ly@users.noreply.github.com>
2025-12-09 01:55:44 +0100

    static menus

--- Assets/Scripts/UI/CanvasInit.cs
+++ Assets/Scripts/UI/CanvasInit.cs
@@ -2,19 +2,39 @@ using UnityEngine;
 
 namespace TeleopReachyXR
 {
+    [RequireComponent(typeof(Canvas))]
     public class CanvasInit : MonoBehaviour
     {
-        [SerializeField]
-        private float PlaneDistance;
+        [Tooltip("Distance in front of the camera where the menu will spawn.")]
+        public float distanceFromCamera = 2f;
 
-        void Start()
+        [Tooltip("If true, the canvas will be placed in front of the camera once on Start.")]
+        public bool placeOnceInFrontOfCamera = true;
+
+        public Camera eventCamera;
+
+        private void Start()
         {
-            // Assigne la caméra de Basescene au canva courant
-            transform.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceCamera;
-            transform.GetComponent<Canvas>().worldCamera = Camera.main;
+            var canvas = GetComponent<Canvas>();
+            canvas.renderMode = RenderMode.WorldSpace;
 
-            if (PlaneDistance != 0) transform.GetComponent<Canvas>().planeDistance = PlaneDistance;
+            Camera cam = eventCamera != null ? eventCamera : Camera.main;
+            if (cam != null)
+            {
+                canvas.worldCamera = cam;
+
+                if (placeOnceInFrontOfCamera)
+                {
+                    // Position 2m in front of camera
+                    Transform ct = cam.transform;
+                    Vector3 forwardFlat = new Vector3(ct.forward.x, 0f, ct.forward.z).normalized;
+                    if (forwardFlat.sqrMagnitude < 0.0001f)
+                        forwardFlat = ct.forward;
+
+                    transform.position = ct.position + forwardFlat * distanceFromCamera;
+                    transform.rotation = Quaternion.LookRotation(forwardFlat, Vector3.up);
+                }
+            }
         }
-
     }
 }
